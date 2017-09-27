afcd07854bbf0e360e4f212aac77b447211b5df1
Aaron Miles <cool_jingle@hotmail.co.uk>
2017-09-27 16:43:53 +0100

    skip presentation if there is copytyping

--- Memrise_Wrong_Answer_Skips.user.js
+++ Memrise_Wrong_Answer_Skips.user.js
@@ -4,85 +4,23 @@
 // @description    Cut back on certain extra screens and item repeats when you answer incorrectly
 // @match          https://www.memrise.com/course/*/garden/*
 // @match          https://www.memrise.com/garden/review/*
-// @version        0.0.6
+// @version        0.0.7
 // @updateURL      https://github.com/cooljingle/memrise-wrong-answer-skips/raw/master/Memrise_Wrong_Answer_Skips.user.js
 // @downloadURL    https://github.com/cooljingle/memrise-wrong-answer-skips/raw/master/Memrise_Wrong_Answer_Skips.user.js
 // @grant          none
 // ==/UserScript==
 
 $(document).ready(function() {
-
-    //edit the below values to change what happens when you answer incorrectly
-    var skipPresentation = true; //screen showing mem
-    var skipCopytyping = false; //screen for typing the correct answer
-    var maxTestAdds = 1; //number of test boxes that get added when you answer incorrectly; standard is 3
-
     MEMRISE.garden.boxes.load = (function() {
         var cached_function = MEMRISE.garden.boxes.load;
         return function() {
             MEMRISE.garden.boxes.add_next = (function() {
                 var cached_function = MEMRISE.garden.boxes.add_next;
-                var testAddCount = 0;
-                var previousIsTestBox;
                 return function() {
-                    var result,
-                        boxType = arguments[0].template,
-                        isTestBox = ["presentation", "copytyping"].indexOf(boxType) === -1;
-                    if(isTestBox) {
-                        testAddCount++;
-                    } else {
-                        testAddCount = 0;
-                    }
-
-                    var shouldSkip = ((skipPresentation && boxType === "presentation") ||
-                        (skipCopytyping && boxType === "copytyping") ||
-                        testAddCount > maxTestAdds) && !arguments[0].isFakeInsert;
-
-                    if(!shouldSkip){
-                        var args = arguments;
-                        if(isTestBox && args[1]) { //arguments[1] represents the chosen number of places from the current box to insert a new test box
-                            //I've pushed the boxes further along in the case of adding less boxes than the standard amount, you can change this to suit your tastes
-                            switch(maxTestAdds){
-                                case 1:
-                                    args[1] = (args[1] + 2)* 2;
-                                    break;
-                                case 2:
-                                    switch(testAddCount){
-                                        case 1:
-                                            args[1] = args[1] * 2;
-                                            break;
-                                        case 2:
-                                            args[1] = args[1] * 2;
-                                            break;
-                                        default:
-                                            break;
-                                    }
-                                    break;
-                                case 3:
-                                    switch(testAddCount){
-                                        case 1:
-                                            args[1] = args[1]; //corresponds to random(3,7)
-                                            break;
-                                        case 2:
-                                            args[1] = args[1]; //corresponds to first add distance + random(2,6)
-                                            break;
-                                        case 3:
-                                            args[1] = args[1]; //corresponds to second add distance + random(2,6)
-                                            break;
-                                        default:
-                                            break;
-                                    }
-                                    break;
-                                default:
-                                    break;
-                            }
-                        } else if(skipPresentation && args[0].template === "copytyping"){
-                            args[1] = 0;
-                        }
-                        result = cached_function.apply(this, args);
-                    } else {
-                        return result;
-                    }
+                    var result = cached_function.apply(this, arguments);
+                    if(arguments[0].template === "copytyping" && MEMRISE.garden.boxes._list[MEMRISE.garden.boxes.num + 1].template === "presentation")
+                        MEMRISE.garden.boxes._list.splice(MEMRISE.garden.boxes.num + 1, 1);
+                    return result;
                 };
             }());
             return cached_function.apply(this, arguments);
